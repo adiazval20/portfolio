52729786f32e1beb0e96b71d31f2e7eb26fd2f7d
Andy Diaz <adiazval20@gmail.com>
2025-11-23 13:19:31 -0500

    1.0

---
+++ server.js
@@ -0,0 +1,24 @@
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+
+const dev = false;
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const PORT = process.env.PORT || 3000;
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true);
+      await handle(req, res, parsedUrl);
+    } catch (err) {
+      console.error('Error handling request:', err);
+      res.statusCode = 500;
+      res.end('Internal server error');
+    }
+  }).listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${PORT}`);
+  });
+});
