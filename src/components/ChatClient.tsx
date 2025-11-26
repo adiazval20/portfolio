b3a293ec49df23846b35f9942ae01ff642b7f2f9
Andy Diaz <adiazval20@gmail.com>
2025-11-25 21:18:57 -0500

    Style fixes

--- src/components/ChatClient.tsx
+++ src/components/ChatClient.tsx
@@ -27,7 +27,12 @@ export default function ChatClient({ apiEndpoint, onClose }: ChatClientProps) {
                                 name="Andy"
                                 className="border-2 border-gray-400 my-2 ml-4"
                                 src="avatar_circle.png" />
-                            <div className="py-2 pr-4 pl-4 rounded-bl-2xl bg-white">
+                            <div>
+                                {typing &&
+                                    <TypingIndicator content="Andy is typing" />
+                                }
+                            </div>
+                            <div className="py-2 pr-4 pl-4 rounded-bl-2xl bg-white self-start">
                                 <button onClick={onClose} className="rounded-full p-1 border-2 border-gray-400 hover:bg-gray-100 hover:border-gray-500 cursor-pointer">
                                     <svg className="w-5 h-5 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18 17.94 6M18 18 6.06 6" />
@@ -40,9 +45,7 @@ export default function ChatClient({ apiEndpoint, onClose }: ChatClientProps) {
                         {messages.map((m, i) => (
                             <Message key={i} model={m} />
                         ))}
-                        {typing &&
-                            <TypingIndicator content="Andy is typing" />
-                        }
+
                     </MessageList>
                     <MessageInput
                         placeholder="Ask about me!"
